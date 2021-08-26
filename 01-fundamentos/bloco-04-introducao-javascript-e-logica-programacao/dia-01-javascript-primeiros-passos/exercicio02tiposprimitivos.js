bc999f5faaacceb8e246590898fcba3eea7a27af
Elton <eltonmorenocl@gmail.com>
2021-08-25 21:02:59 -0300

    exercicio 2

---
+++ 01-fundamentos/bloco-04-introducao-javascript-e-logica-programacao/dia-01-javascript-primeiros-passos/exercicio02tiposprimitivos.js
@@ -0,0 +1,22 @@
+let patientId = 50;
+let isEnrolled = true;
+const patientInfo = {
+    firstName: 'Ana',
+    lastName: 'Santos',
+};
+const patientEmail = 'ana@email.com';
+
+
+console.log(typeof patientId);
+console.log(typeof isEnrolled);
+console.log(typeof patientInfo);
+console.log(typeof patientEmail);
+console.log(typeof patientAge);
+
+// Todos os tipo primitivos
+
+const base = 5;
+const height = 8;
+const area = base * height
+
+console.log(area);
