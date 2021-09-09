e8b4544c7227590b01ed98aaa27da3fa7bbe26b6
elton <eltonmorenocl@gmail.com>
2021-09-09 00:03:48 -0300

    comitando arquivos

--- 01-fundamentos/bloco-05-javascript-dom-eventos-webstorage/dia-03-javascript-eventos/exercicio01addeventlistener/main.js
+++ 01-fundamentos/bloco-05-javascript-dom-eventos-webstorage/dia-03-javascript-eventos/exercicio01addeventlistener/main.js
@@ -10,8 +10,14 @@ const myWebpage = document.getElementById('my-spotrybefy');
 // 2. Crie uma função que adicione a classe 'tech' ao elemento selecionado;
 // 2.1. Deve existir apenas um elemento com a classe 'tech'. Como você faz isso?
 
+function name(params) {
+  
+}
+
+
 // 3. Crie uma função que, ao digitar na caixa de texto, altere o texto do elemento
 // com a classe 'tech';
+
 
 // 4. Crie uma função que, ao clicar duas vezes em 'Meu top 3 do Spotrybefy', ele
 // redirecione para alguma página;
