bfd088b6ada034fc6d4b64fcd0fff1f9bc9aea25
elton <eltonmorenocl@gmail.com>
2021-10-07 12:29:09 -0300

    pratica

--- 01-fundamentos/bloco-08-js-higher-order-functions/dia-03-map-filter/pratica/pratica-05.js
+++ 01-fundamentos/bloco-08-js-higher-order-functions/dia-03-map-filter/pratica/pratica-05.js
@@ -61,21 +61,15 @@ const books = [
     },
   ];
   
-//   1 - Crie um array com strings no formato NOME_DO_LIVRO - GÊNERO_DO_LIVRO - NOME_DA_PESSOA_AUTORA
-//   Dica: Use a função map
+//   5 - Crie um array em ordem alfabética apenas com os nomes de todas as pessoas autoras de ficção científica ou fantasia.
 
 const expectedResult = [
-    'As Crônicas de Gelo e Fogo - Fantasia - George R. R. Martin',
-    'O Senhor dos Anéis - Fantasia - J. R. R. Tolkien',
-    'Fundação - Ficção Científica - Isaac Asimov',
-    'Duna - Ficção Científica - Frank Herbert',
-    'A Coisa - Terror - Stephen King',
-    'O Chamado de Cthulhu - Terror - H. P. Lovecraft',
+    'Frank Herbert',
+    'George R. R. Martin',
+    'Isaac Asimov',
+    'J. R. R. Tolkien',
   ];
   
-  function formatedBookNames() {
-    const booksArr = books.map((book) => {
-      book.name 
-  })
-  console.log(booksArr);
-  };
+  function fantasyOrScienceFictionAuthors() {
+    // escreva seu código aqui
+  }
