8453ede2ffdec1f9a5a31510a426ea28dbeff519
elton <eltonmorenocl@gmail.com>
2021-10-25 19:10:34 -0300

    exercicios

---
+++ 01-fundamentos/bloco-09-javascript-testes-assincronos/dia-03-jest-testes-assincronos/async-await-exemplo1.js
@@ -0,0 +1,13 @@
+// getData.js
+const fetch = require('node-fetch');
+
+const getSuperHero = () => {
+  const result = fetch('https://www.superheroapi.com/api.php/4192484924171229/720')
+    .then((response) => response.json())
+    .then((hero) => (hero.name));
+  return result;
+};
+
+getSuperHero(); // Wonder Woman
+
+module.exports = getSuperHero;
